12efa8bd9a8dd764c30e62116fafd6a0488480a6
zervis <zervis@gmail.com>
2021-01-13 11:24:08 +0100

    final

--- apps/info_sys/lib/info_sys/wolfram.ex
+++ apps/info_sys/lib/info_sys/wolfram.ex
@@ -12,6 +12,8 @@ defmodule InfoSys.Wolfram do
 
     @impl true
 
+    @http Application.get_env(:info_sys, :wolfram)[:http_client] || :httpc
+
     def compute(query_str, _opts) do
         query_str
         |> fetch_xml()
@@ -26,8 +28,7 @@ defmodule InfoSys.Wolfram do
     end
 
     defp fetch_xml(query) do
-        {:ok, {_, _, body}} = :httpc
-        .request(String.to_charlist(url(query)))
+        {:ok, {_, _, body}} = @http.request(String.to_charlist(url(query)))
 
         body
     end
@@ -36,5 +37,6 @@ defmodule InfoSys.Wolfram do
         "#{@base}?" <> URI.encode_query(appid: id(), input: input, format: "plaintext")
     end
 
-    defp id, do: Application.fetch_env!(:info_sys, :wolfram)[:app_id]
+    defp id, do: Application.fetch_env!(:info_sys, :wolfram)[:app_id] 
+
 end
